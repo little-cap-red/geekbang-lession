d0610d75b7045a77726c2aa98a43ad26da3ec6fc
gmy_zhangjie <zhangjie@guanmaoyun.com>
2020-09-16 15:09:52 +0800

    same thing

---
+++ geekbang-lessons/spring-bean/src/main/java/org/geekbang/thinking/in/spring/bean/definition/BeanDefinitionCreationDemo.java
@@ -0,0 +1,18 @@
+package org.geekbang.thinking.in.spring.bean.definition;
+
+import org.geekbang.thinking.in.spring.ioc.overview.domain.User;
+import org.springframework.beans.factory.config.BeanDefinition;
+/**
+ * {@link BeanDefinition} BeanDefinition 构建示例
+ */
+public class BeanDefinitionCreationDemo {
+
+    public static void main(String[] args) {
+
+        //1.通过 BeanDefinitionBuilder 构建
+        User user = null;
+
+    }
+
+
+}
